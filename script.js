929e687e462c0bc72d254002acee62010f3cc61b
Jyoti Das <48188772+CodeSTACKE@users.noreply.github.com>
2022-02-01 03:19:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+  let texts =[ 
+    "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+    "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...",
+    "Curabitur iaculis, nisl ut tristique fringilla, orci purus rutrum arcu, sed pharetra lectus tortor eget orci."
+]
+ let bigText="";
+ let longest_word="";
+function convertToUpperCase(text){
+//convert the first letter of each word to upper case
+  let array1 = text.split(' ');
+  let newarray1 = [];
+    
+  for(let x = 0; x < array1.length; x++){
+      newarray1.push(array1[x].charAt(0).toUpperCase()+array1[x].slice(1));
+  }
+   bigText = newarray1.join(' ');
+   console.log(bigText);
+}
+
+
+function longestWord(text){
+//find the longest word
+array1 = text.match(/\w[a-z]{0,}/gi);
+ longest_word = array1[0];
+
+  for(let x = 1 ; x < array1.length ; x++)
+  {
+    if(longest_word.length < array1[x].length)
+    {
+    longest_word = array1[x];
+    } 
+  }
+}
+
+function vowelCounting(text){
+//count number of vowels
+let vowel_list = 'aeiouAEIOU';
+let vcount = 0;
+
+for(let x = 0; x < text.length ; x++)
+{
+  if (vowel_list.indexOf(text[x]) !== -1)
+  {
+    vcount += 1;
+  }
+}
+
+console.log(`The original string was:\n${text}\nCapitalizing each word looks like:\n${bigText}\nThe longest word is ${longest_word}\nThere are ${vcount} vowels`);
+}
+for(let i=0;i<texts.length;i++){
+convertToUpperCase(texts[i]);
+
+}
+
